1aea7611b146af51614c270568f865be5ff18f22
chokhrich-dot <chokhrich@gmail.com>
2025-12-24 20:42:49 +0000

    Create ZamaCCOEvents.sol

---
+++ contracts/ZamaCCOEvents.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ZamaCCOEvents {
+    event BorrowerProfileSubmitted(address indexed borrower);
+    event CreditScoreComputed(address indexed borrower);
+}
